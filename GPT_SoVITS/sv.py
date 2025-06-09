6ac144a1b42ba9b5cd3ab9c0b2e958b4bcbc8812
Jarod Mica <jarodmica@gmail.com>
2025-06-08 20:50:46 -0700

    Update sv.py paths

--- GPT_SoVITS/sv.py
+++ GPT_SoVITS/sv.py
@@ -1,10 +1,10 @@
-import sys,os,torch
-sys.path.append(f"{os.getcwd()}/GPT_SoVITS/eres2net")
-sv_path = "GPT_SoVITS/pretrained_models/sv/pretrained_eres2netv2w24s4ep4.ckpt"
-from ERes2NetV2 import ERes2NetV2
-import kaldi as Kaldi
+import torch
+from GPT_SoVITS.eres2net.ERes2NetV2 import ERes2NetV2
+import GPT_SoVITS.eres2net.kaldi as Kaldi
 class SV:
-    def __init__(self,device,is_half):
+    def __init__(self,device,is_half, sv_path=None):
+        if sv_path is None:
+            sv_path = "GPT_SoVITS/pretrained_models/sv/pretrained_eres2netv2w24s4ep4.ckpt"
         pretrained_state = torch.load(sv_path, map_location='cpu', weights_only=False)
         embedding_model = ERes2NetV2(baseWidth=24,scale=4,expansion=4)
         embedding_model.load_state_dict(pretrained_state)
